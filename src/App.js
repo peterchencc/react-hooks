6e5113f94ac60131d7ca258c4c88d187896c1aa3
peterchencc <peterchencc@gmail.com>
2020-04-01 16:25:49 +0800

    setup userTable view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import UserTable from './tables/UserTable'
 
-function App() {
+const App = () => {
+  const usersData = [
+    { id: 1, name: 'Peter Chen', username: 'peterchen' },
+    { id: 2, name: 'Peter Chen 1', username: 'peterchen1' },
+    { id: 3, name: 'Peter Chen 2', username: 'peterchen2' }
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
